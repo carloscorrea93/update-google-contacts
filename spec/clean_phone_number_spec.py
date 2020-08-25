53e8a9b18da2c190bc70adc42ad6883a51cafcf2
Carlos Correa <carlos@fondeadora.com>
2020-08-24 23:46:01 -0500

    feat: add function to clean mx patterns

--- spec/clean_phone_number_spec.py
+++ spec/clean_phone_number_spec.py
@@ -1,7 +1,7 @@
 from expects import equal, expect
 from mamba import description, it
 
-from src.utils import clean_phone_number
+from src.utils import clean_mx_with_regex, clean_phone_number
 
 with description(clean_phone_number) as self:
 
@@ -16,3 +16,18 @@ with description(clean_phone_number) as self:
     with it('clean with whitespaces inside'):
         result = clean_phone_number('+52 55 10 30 00 00')
         expect(result).to(equal('+525510300000'))
+
+
+with description(clean_mx_with_regex) as self:
+
+    with it('clean with +521'):
+        result = clean_mx_with_regex('+5214491121594')
+        expect(result).to(equal('4491121594'))
+
+    with it('clean with +52'):
+        result = clean_mx_with_regex('+524491121594')
+        expect(result).to(equal('4491121594'))
+
+    with it('clean with whitespaces inside'):
+        result = clean_mx_with_regex('4491121594')
+        expect(result).to(equal('4491121594'))
--- src/utils/clean_phone_number.py
+++ src/utils/clean_phone_number.py
@@ -1,2 +1,9 @@
+import re
+
+
 def clean_phone_number(number):
     return number.strip().replace(' ', '')
+
+
+def clean_mx_with_regex(number):
+    return re.sub(r'^\+521?', '', number)
